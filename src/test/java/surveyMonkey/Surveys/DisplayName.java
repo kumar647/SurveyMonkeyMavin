1b634ed57428a2be8df9febb19b6d723c90dd96b
Naresh Kumar <kumar647@outlook.com>
2018-08-28 23:10:35 -0400

    Naresh

---
+++ src/test/java/surveyMonkey/Surveys/DisplayName.java
@@ -0,0 +1,14 @@
+package surveyMonkey.Surveys;
+
+public class DisplayName {
+
+	public static void main(String[] args) {
+		displayName();
+
+	}
+	
+	public static void displayName() {
+		System.out.println("Naresh");
+	}
+
+}
---
+++ src/test/java/surveyMonkey/Surveys/HelloJava.java
@@ -0,0 +1,14 @@
+package surveyMonkey.Surveys;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		displayMessage();
+
+	}
+	
+	public static void displayMessage() {
+		System.out.println("Hello World");
+	}
+
+}
